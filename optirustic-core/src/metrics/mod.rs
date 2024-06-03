13786e6fade9ee6f6b94aa0b4903890cc79afde0
Stefano Simonelli <16114781+s-simoncelli@users.noreply.github.com>
2024-06-03 06:07:47 +0100

    Use workspace

--- optirustic-core/src/metrics/mod.rs
+++ optirustic-core/src/metrics/mod.rs
@@ -1,5 +1,7 @@
 pub use hypervolume::estimate_reference_point;
 pub use hypervolume_2d::HyperVolume2D;
+pub use hypervolume_3d::HyperVolume3D;
 
 pub mod hypervolume;
 pub mod hypervolume_2d;
+mod hypervolume_3d;
