337ea59b1eb4fb72b5d97dfddb5a1776ef871cd3
Stefano Simonelli <16114781+s-simoncelli@users.noreply.github.com>
2024-09-01 09:16:40 +0100

    Added Adaptive NSGA3 tests with inverted DTLZ1 problem

--- src/algorithms/a_nsga3.rs
+++ src/algorithms/a_nsga3.rs
@@ -24,3 +24,80 @@ impl AdaptiveNSGA3 {
         NSGA3::new(problem, options, true)
     }
 }
+
+#[cfg(test)]
+mod test_problems {
+    use float_cmp::approx_eq;
+
+    use optirustic_macros::test_with_retries;
+
+    use crate::algorithms::{
+        AdaptiveNSGA3, Algorithm, MaxGeneration, NSGA3Arg, Nsga3NumberOfIndividuals,
+        StoppingConditionType,
+    };
+    use crate::core::builtin_problems::DTLZ1Problem;
+    use crate::core::test_utils::assert_approx_array_eq;
+    use crate::operators::{PolynomialMutationArgs, SimulatedBinaryCrossoverArgs};
+    use crate::utils::NumberOfPartitions;
+
+    #[test_with_retries(10)]
+    /// Test the inverted DTLZ1 problem with M=3 and MaxGeneration = 400
+    fn test_inverted_dtlz1_obj_3() {
+        let k: usize = 5;
+        let number_variables: usize = 3 + k - 1;
+        let problem = DTLZ1Problem::create(number_variables, 3, true).unwrap();
+        let number_of_partitions = NumberOfPartitions::OneLayer(12);
+        let pop_size: usize = 92;
+        let expected_ref_points: usize = 91;
+
+        let crossover_operator_options = SimulatedBinaryCrossoverArgs {
+            distribution_index: 30.0,
+            ..SimulatedBinaryCrossoverArgs::default()
+        };
+        let mutation_operator_options = PolynomialMutationArgs::default(&problem);
+
+        let args = NSGA3Arg {
+            number_of_individuals: Nsga3NumberOfIndividuals::Custom(pop_size),
+            number_of_partitions,
+            crossover_operator_options: Some(crossover_operator_options),
+            mutation_operator_options: Some(mutation_operator_options),
+            stopping_condition: StoppingConditionType::MaxGeneration(MaxGeneration(400)),
+            parallel: None,
+            export_history: None,
+            seed: Some(1),
+        };
+
+        let mut algo = AdaptiveNSGA3::new(problem, args).unwrap();
+        assert_eq!(algo.reference_points().len(), expected_ref_points);
+
+        algo.run().unwrap();
+        let results = algo.get_results();
+
+        let expected_vars = vec![0.5; number_variables];
+        let mut invalid_individuals: usize = 0;
+        for ind in results.individuals {
+            let obj_sum: f64 = ind.get_objective_values().unwrap().iter().sum();
+            // objective target sum is 1
+            let outside_range_data = approx_eq!(f64, obj_sum, 1.0, epsilon = 0.01);
+            if !outside_range_data {
+                invalid_individuals += 1;
+            }
+
+            // All variables in x_M must be 0.5
+            let vars: Vec<f64> = ((number_variables - k + 1)..=number_variables)
+                .map(|i| {
+                    ind.get_variable_value(format!("x{i}").as_str())
+                        .unwrap()
+                        .as_real()
+                        .unwrap()
+                })
+                .collect();
+            assert_approx_array_eq(&vars, &expected_vars, Some(0.01));
+        }
+
+        // about 90% of solutions are ideal
+        if invalid_individuals > 10 {
+            panic!("Found {invalid_individuals} individuals not meeting the ideal solution");
+        }
+    }
+}
